aa730f48726fb52941af2d7f87f33c690385b0b0
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-07-30 21:44:13 +0300

    Solving shadcn ui components

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
 
+
 const AuthLayout = () => {
   return (
     <div>
       <h1>I'm The Layout</h1>
-      <Outlet/>
+      <section>
+        <Outlet/>
+      </section>
     </div>
   )
 }
